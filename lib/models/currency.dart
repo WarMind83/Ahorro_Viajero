01684eb712b82f66b944aa0e2efa99b41fd6fb35
WarMind83 <drwargos@gmail.com>
2025-04-21 12:51:01 +0200

    Agregar modelo Currency

--- lib/models/currency.dart
+++ lib/models/currency.dart
@@ -8,16 +8,20 @@ class Currency {
     required this.name,
     required this.symbol,
   });
-  
-  // Lista predefinida de monedas comunes
-  static List<Currency> commonCurrencies = [
-    Currency(code: 'EUR', name: 'Euro', symbol: '€'),
-    Currency(code: 'USD', name: 'Dólar estadounidense', symbol: '\$'),
-    Currency(code: 'GBP', name: 'Libra esterlina', symbol: '£'),
-    Currency(code: 'JPY', name: 'Yen japonés', symbol: '¥'),
-    Currency(code: 'MXN', name: 'Peso mexicano', symbol: '\$'),
-    Currency(code: 'CAD', name: 'Dólar canadiense', symbol: '\$'),
-    Currency(code: 'AUD', name: 'Dólar australiano', symbol: '\$'),
-    Currency(code: 'CHF', name: 'Franco suizo', symbol: 'Fr'),
-  ];
+
+  factory Currency.fromJson(Map<String, dynamic> json) {
+    return Currency(
+      code: json['code'],
+      name: json['name'],
+      symbol: json['symbol'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'code': code,
+      'name': name,
+      'symbol': symbol,
+    };
+  }
 }
